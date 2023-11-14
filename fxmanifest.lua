a2ca0b4e93c6c4dc4f77c30b9b5835e5cb7e9682
Tanvir Anjum Neon <taneon98@gmail.com>
2023-11-14 08:54:13 +0600

    Hud

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Mr_Dx'
+author 'mr_dx'
+description 'Mr_Dx Scripts'
+version '2.0'
+url 'https://github.com/Mr-Dx-Developer'
+
+-- Files
+client_script 'Dx.Client.net.dll'
+ui_page 'html/ui.html'
+
+server_script 'HttpRequests.lua'
+server_script 'Dx.Server.net.dll'
+
+
+files {
+    'Newtonsoft.Json.dll',
+    'config/config.json',
+    'config/postals.json',
+    'config/displays.json',
+}
